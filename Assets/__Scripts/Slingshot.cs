96b13a968e0f7aea5e28c33ff5208e548430ee35
Leonard Leyva <ll788@scarletmail.rutgers.edu>
2019-10-03 15:18:49 -0400

    Launchpoint scene

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -6,13 +6,35 @@ using UnityEngine;
 public class Slingshot : MonoBehaviour {
 
     // class level variable
+    [Header("Set in variable")]
+    public GameObject prefabProjectile;
+
+    [Header("Set dynamically")]
     public GameObject launchPoint;
 
-    private void Awake()
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+
+    void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    } // end of Awake method
+    private void OnMouseDown()
+    {
+        // the player has pressed the mouse button while over slingshot
+        aimingMode = true;
+        // instantiating a projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        // start it at the launchPoint
+        projectile.transform.position = launchPos;
+        // set it to is Kinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+
     }
 
     // Use this for initialization
