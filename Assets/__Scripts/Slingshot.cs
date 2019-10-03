cc3c75f6a8ab36f3e0e7948d61636782ec67f0d6
Leonard Leyva <ll788@scarletmail.rutgers.edu>
2019-10-03 16:32:21 -0400

    projectile

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -7,11 +7,11 @@ public class Slingshot : MonoBehaviour {
 
     // class level variable
     [Header("Set in variable")]
-    public GameObject prefabProjectile;
+    public GameObject prefabProjectile;   // set in Inspectator
     public float velocityMult = 8f;
 
     [Header("Set dynamically")]
-    public GameObject launchPoint;
+    public GameObject launchPoint;       // 3d world pos of launchPoint
 
     public Vector3 launchPos;
     public GameObject projectile;
@@ -28,10 +28,42 @@ public class Slingshot : MonoBehaviour {
         launchPoint.SetActive(false);
         launchPos = launchPointTrans.position;
     } // end of Awake method
-    private void Update()
+     void Update()
     {
         // if slingshot is not in aimingMode, don't run the code
         if (!aimingMode) return;      // bad code, bad
+        // get the current mouse position in 2D screen coordinates
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        // find the delta from the launchPos to the mousePos3D
+        Vector3 mouseDelta = mousePos3D - launchPos;    // vectorsubtraction
+        // Limit mouseDelta to the radius of the slingshot sphereCollider
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }  // end of if
+
+        //move the projectile to this new position
+        Vector3 projPos = launchPos + mouseDelta;    // vector addition
+        projectile.transform.position = projPos;     // move our projectile to the new position
+
+        if (Input.GetMouseButton(0))
+        {
+            // the mouse has been released
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            projectile = null;
+        }  // end of if
+
+           
+
+
+
     }
 
     void OnMouseDown()
